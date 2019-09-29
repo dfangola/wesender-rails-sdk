2d271f3778309167cdbe0ed812a5bef33b3c725e
cabanga <manucabanga@gmail.com>
2019-09-29 22:01:27 +0100

    add project gem

---
+++ lib/wesender/rails.rb
@@ -0,0 +1,8 @@
+require "wesender/rails/version"
+
+module Wesender
+  module Rails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/wesender/rails/version.rb
@@ -0,0 +1,5 @@
+module Wesender
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
