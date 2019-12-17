df50d366e62882e8cb81f25ddde055f00fd15e82
cabanga <manucabanga@gmail.com>
2019-12-17 18:05:01 +0100

    config httpParty

--- lib/wesender.rb
+++ lib/wesender.rb
@@ -1,8 +1,39 @@
 require "wesender/version"
+require "httparty"
 
 module Wesender
+  include HTTParty
 
-
-
+  base_uri "http://apiwesender.digitalfactory.co.ao"
   
+  def initialize(api_key)
+    @api_key = api_key
+  end
+
+  def self.sendSMS(numbers, message, optins = {})
+    content = {}
+    content[:headers] = {'Content-Type' => 'application/json'}
+    content[:body] = {
+      ApiKey: @api_key,
+      Destino: numbers,
+      Mensagem: message,
+      CEspecial: false
+    }
+    
+    puts content
+    
+    post("/envio/#{@api_key}", content)
+  end
+
+
+  #def self.set_base_url(is_test = false)
+  #  self.base_uri is_test == true ? "http://apiwesender.digitalfactory.co.ao" : "https://app.wesender.co.ao"
+  #end
+
 end
+
+
+#ApiKey: ,
+#Destino: ,
+#Mensagem: ,
+#CEspecial: false
